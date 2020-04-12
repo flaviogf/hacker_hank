e6d9b9a8a51a10bf46c1ee9641db77b79306707e
flaviogf <flavio.fernandes6@gmail.com>
2020-04-12 14:48:21 -0300

    feat: compare the triplets

---
+++ compare_the_triplets/CompareTheTriplets.Answer/Resolution.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace CompareTheTriplets.Answer
+{
+    public class Resolution
+    {
+        public List<int> Execute(List<int> itemsA, List<int> itemsB)
+        {
+            return Enumerable
+                    .Zip(itemsA, itemsB, (a, b) => new { a, b })
+                    .Aggregate(new List<int> { 0, 0 }, (acc, it) =>
+                    {
+                        if (it.a > it.b)
+                        {
+                            acc[0]++;
+                        }
+                        else if (it.a < it.b)
+                        {
+                            acc[1]++;
+                        }
+
+                        return acc;
+                    });
+        }
+    }
+}
---
+++ compare_the_triplets/CompareTheTriplets.Test/ResolutionTest.cs
@@ -0,0 +1,21 @@
+﻿using CompareTheTriplets.Answer;
+using System.Collections.Generic;
+using Xunit;
+
+namespace CompareTheTriplets.Test
+{
+    public class ResolutionTest
+    {
+        [Fact]
+        public void Should_Return_2_and_1_When_The_First_List_Has_The_Numbers_17_28_30_and_The_Second_List_Has_The_Numbers_99_16_8()
+        {
+            var a = new List<int> { 17, 28, 30 };
+
+            var b = new List<int> { 99, 16, 8 };
+
+            var resolution = new Resolution();
+
+            Assert.Equal(new List<int> { 2, 1 }, resolution.Execute(a, b));
+        }
+    }
+}
