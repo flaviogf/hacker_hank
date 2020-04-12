8c18032e03dcf8f9b8507828fc031b367d2eb86d
flaviogf <flavio.fernandes6@gmail.com>
2020-04-12 13:07:51 -0300

    feat: simple_array_sum

---
+++ simple_array_sum/SimpleArraySum.Answer/Resolution.cs
@@ -0,0 +1,12 @@
+﻿using System.Linq;
+
+namespace SimpleArraySum.Answer
+{
+    public class Resolution
+    {
+        public int Execute(params int[] items)
+        {
+            return items.Sum();
+        }
+    }
+}
---
+++ simple_array_sum/SimpleArraySum.Test/ResolutionTest.cs
@@ -0,0 +1,16 @@
+﻿using SimpleArraySum.Answer;
+using Xunit;
+
+namespace SimpleArraySum.Test
+{
+    public class ResolutionTest
+    {
+        [Fact]
+        public void Should_Return_31_When_Is_Informed_The_Numbers_1_2_3_4_10_and_11()
+        {
+            var resolution = new Resolution();
+
+            Assert.Equal(31, resolution.Execute(1, 2, 3, 4, 10, 11));
+        }
+    }
+}
