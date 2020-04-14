71093cbdec39224d4f0e8ddaa18a9b0361cb6ba8
flaviogf <flavio.fernandes6@gmail.com>
2020-04-13 21:00:05 -0300

    feat: staircase

---
+++ staircase/Staircase.Answer/Resolution.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Linq;
+
+namespace Staircase.Answer
+{
+    public class Resolution
+    {
+        public string[] Execute(int count)
+        {
+            var result = Enumerable.Range(0, count).Select((it, index) => new string('#', index + 1).PadLeft(count, ' ')).ToArray();
+
+            foreach (var it in result)
+            {
+                Console.WriteLine(it);
+            }
+
+            return result;
+        }
+    }
+}
---
+++ staircase/Staircase.Test/ResolutionTest.cs
@@ -0,0 +1,24 @@
+﻿using Staircase.Answer;
+using Xunit;
+
+namespace Staircase.Test
+{
+    public class ResolutionTest
+    {
+        [Fact]
+        public void Execute_WhenIsExecutedWithCountFour_ShouldReturnTheStaircase()
+        {
+            var resolution = new Resolution();
+
+            var expected = new string[]
+            {
+                "   #",
+                "  ##",
+                " ###",
+                "####"
+            };
+
+            Assert.Equal(expected, resolution.Execute(4));
+        }
+    }
+}
