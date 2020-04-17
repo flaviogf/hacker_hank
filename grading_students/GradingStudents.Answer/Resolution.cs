817f2327b8f05c2681b3ede6331bb3b101fe4bb2
flaviogf <flavio.fernandes6@gmail.com>
2020-04-16 22:19:31 -0300

    feat: grading students

---
+++ grading_students/GradingStudents.Answer/Resolution.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace GradingStudents.Answer
+{
+    public class Resolution
+    {
+        public List<int> Execute(List<int> grades)
+        {
+            return grades.Select(Round).ToList();
+        }
+
+        public int Round(int grade)
+        {
+            if (grade < 38)
+            {
+                return grade;
+            }
+
+            int nextMultiplyOf5 = NextMultiplyOf5(grade);
+
+            if (nextMultiplyOf5 - grade < 3)
+            {
+                return nextMultiplyOf5;
+            }
+
+            return grade;
+        }
+
+        private int NextMultiplyOf5(int number)
+        {
+            return number - (number % 5) + 5;
+        }
+    }
+}
---
+++ grading_students/GradingStudents.Test/ResolutionTest.cs
@@ -0,0 +1,23 @@
+﻿using GradingStudents.Answer;
+using System.Collections.Generic;
+using Xunit;
+
+namespace GradingStudents.Test
+{
+    public class ResolutionTest
+    {
+        public static IEnumerable<object[]> GetGrades()
+        {
+            yield return new object[] { new List<int> { 73, 67, 38, 33 }, new List<int> { 75, 67, 40, 33 } };
+        }
+
+        [Theory]
+        [MemberData(nameof(GetGrades))]
+        public void Execute_When_Given_An_Array_Of_Grades_Should_Return_The_Grades_Rounded(List<int> grades, List<int> expected)
+        {
+            var resolution = new Resolution();
+
+            Assert.Equal(expected, resolution.Execute(grades));
+        }
+    }
+}
