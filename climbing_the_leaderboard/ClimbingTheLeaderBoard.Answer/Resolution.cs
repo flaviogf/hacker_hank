9d9acab1f65495b45e7fcbfeff0b3e023d2fb5bb
flaviogf <flavio.fernandes6@gmail.com>
2020-05-14 13:50:49 -0300

    feat: climbing the leaderboard

--- climbing_the_leaderboard/ClimbingTheLeaderBoard.Answer/Resolution.cs
+++ climbing_the_leaderboard/ClimbingTheLeaderBoard.Answer/Resolution.cs
@@ -1,5 +1,4 @@
 using System;
-using System.Collections.Generic;
 using System.Linq;
 
 namespace ClimbingTheLeaderBoard.Answer
@@ -8,36 +7,23 @@ namespace ClimbingTheLeaderBoard.Answer
     {
         public int[] Execute(int[] scores, int[] player)
         {
-            var ranking = new List<int>();
+            var distinct = scores.Distinct().ToArray();
 
-            var position = 0;
-
-            for (var i = 0; i < scores.Length; i++)
-            {
-                if (position == 0)
-                {
-                    ranking.Add(++position);
-                }
-                else if (scores[i - 1] != scores[i])
-                {
-                    ranking.Add(++position);
-                }
-                else
-                {
-                    ranking.Add(position);
-                }
-            }
-
-            var last = ranking.Last();
-
-            var result = player.Select(p =>
-            {
-                var index = Array.FindIndex(scores, s => s <= p);
-
-                return index == -1 ? last + 1 : ranking[index];
-            });
+            var result = player.Select(p => Position(distinct, p));
 
             return result.ToArray();
+        }
+
+        private static int Position(int[] scores, int score)
+        {
+            var position = Array.FindIndex(scores, (s) => s <= score);
+
+            if (position != -1)
+            {
+                return position + 1;
+            }
+
+            return scores.Length + 1;
         }
     }
 }
