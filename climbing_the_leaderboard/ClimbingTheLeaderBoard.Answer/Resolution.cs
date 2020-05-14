4b564db4ecbf384d7af6d8e87f3490f664f3bc9f
flaviogf <flavio.fernandes6@gmail.com>
2020-05-13 23:11:57 -0300

    feat: climbing the leaderboard

--- climbing_the_leaderboard/ClimbingTheLeaderBoard.Answer/Resolution.cs
+++ climbing_the_leaderboard/ClimbingTheLeaderBoard.Answer/Resolution.cs
@@ -1,12 +1,43 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
 namespace ClimbingTheLeaderBoard.Answer
 {
     public class Resolution
     {
-        public int[] Execute(int[] registeredScores, int[] playerScores)
+        public int[] Execute(int[] scores, int[] player)
         {
-            // TODO
+            var ranking = new List<int>();
 
-            return new int[] { 6, 4, 2, 1 };
+            var position = 0;
+
+            for (var i = 0; i < scores.Length; i++)
+            {
+                if (position == 0)
+                {
+                    ranking.Add(++position);
+                }
+                else if (scores[i - 1] != scores[i])
+                {
+                    ranking.Add(++position);
+                }
+                else
+                {
+                    ranking.Add(position);
+                }
+            }
+
+            var last = ranking.Last();
+
+            var result = player.Select(p =>
+            {
+                var index = Array.FindIndex(scores, s => s <= p);
+
+                return index == -1 ? last + 1 : ranking[index];
+            });
+
+            return result.ToArray();
         }
     }
 }
--- climbing_the_leaderboard/ClimbingTheLeaderBoard.Test/ResolutionTest.cs
+++ climbing_the_leaderboard/ClimbingTheLeaderBoard.Test/ResolutionTest.cs
@@ -10,13 +10,13 @@ namespace ClimbingTheLeaderBoard.Test
         {
             var resolution = new Resolution();
 
-            var registeredScores = new int[] { 100, 100, 50, 40, 40, 20, 10 };
+            var scores = new int[] { 100, 100, 50, 40, 40, 20, 10 };
 
-            var playerScores = new int[] { 5, 25, 50, 120 };
+            var player = new int[] { 5, 25, 50, 120 };
 
             var expected = new int[] { 6, 4, 2, 1 };
 
-            var result = resolution.Execute(registeredScores, playerScores);
+            var result = resolution.Execute(scores, player);
 
             Assert.Equal(expected, result);
         }
