b273bd0ff0d02faefae8acd3891f5e0c832ed2b0
flaviogf <flavio.fernandes6@gmail.com>
2021-07-07 14:37:56 -0300

    between two sets

--- between_two_sets/lib/between_two_sets.rb
+++ between_two_sets/lib/between_two_sets.rb
@@ -1,9 +1,31 @@
 # frozen_string_literal: true
 
+require 'set'
+
 module BetweenTwoSets
   class << self
     def get_total_x(a, b)
-      3
+      dividends = 1.upto(b.last).to_a
+
+      guesses = Set.new
+
+      dividends.each do |dividend|
+        guesses << dividend if a.all? { |divisor| factor?(dividend, divisor) }
+      end
+
+      result = Set.new
+
+      guesses.each do |divisor|
+        result << divisor if b.all? { |dividend| factor?(dividend, divisor) }
+      end
+
+      result.size
+    end
+
+    private
+
+    def factor?(dividend, divisor)
+      (dividend % divisor).zero?
     end
   end
 end
--- between_two_sets/spec/between_two_sets_spec.rb
+++ between_two_sets/spec/between_two_sets_spec.rb
@@ -4,6 +4,22 @@ require 'spec_helper'
 
 RSpec.describe BetweenTwoSets do
   describe '.get_total_x' do
+    context 'when pass [2, 6] and [24, 36]' do
+      let(:a) do
+        [2, 6]
+      end
+
+      let(:b) do
+        [24, 36]
+      end
+
+      subject do
+        BetweenTwoSets.get_total_x a, b
+      end
+
+      it { is_expected.to eq(2) }
+    end
+
     context 'when pass [2, 4] and [16, 32, 96]' do
       let(:a) do
         [2, 4]
