29097077714ab34cd6e969a5e03e753f9b059a4f
flaviogf <flavio.fernandes6@gmail.com>
2021-07-13 20:36:59 -0300

    breaking the records

--- breaking_the_records/lib/breaking_the_records.rb
+++ breaking_the_records/lib/breaking_the_records.rb
@@ -3,7 +3,23 @@
 module BreakingTheRecords
   class << self
     def execute(scores)
-      [2, 4]
+      least_points = 0
+      most_points = 0
+
+      min = scores[0]
+      max = scores[0]
+
+      scores.each do |score|
+        if min > score
+          least_points += 1
+          min = score
+        elsif max < score
+          most_points += 1
+          max = score
+        end
+      end
+
+      [most_points, least_points]
     end
   end
 end
--- breaking_the_records/spec/breaking_the_records_spec.rb
+++ breaking_the_records/spec/breaking_the_records_spec.rb
@@ -13,5 +13,17 @@ RSpec.describe BreakingTheRecords do
 
       it { is_expected.to eq([2, 4]) }
     end
+
+    context 'when pass [3, 4, 21, 36, 10, 28, 35, 5, 24, 42]' do
+      let(:scores) do
+        [3, 4, 21, 36, 10, 28, 35, 5, 24, 42]
+      end
+
+      subject do
+        BreakingTheRecords.execute(scores)
+      end
+
+      it { is_expected.to eq([4, 0]) }
+    end
   end
 end
