2c65b8651eeb5e072694d73d1534a8bd5e760c6c
flaviogf <flavio.fernandes6@gmail.com>
2021-07-14 20:48:43 -0300

    climbing_the_leaderboard

--- climbing_the_leaderboard/lib/climbing_the_leaderboard.rb
+++ climbing_the_leaderboard/lib/climbing_the_leaderboard.rb
@@ -1,9 +1,33 @@
 # frozen_string_literal: true
 
+require 'pry'
+
 module ClimbingTheLeaderBoard
   class << self
-    def execute(scores, players)
-      [6, 5, 4, 2, 1]
+    def execute(ranked, player)
+      positions = Array.new(ranked.size) { 1 }
+
+      position = 1
+
+      ranked.each_with_index do |_, i|
+        position += 1 if ranked[i] < ranked[i - 1]
+        positions[i] = position
+      end
+
+      result = []
+
+      for i in 0...player.size
+        position = positions.last + 1
+        for j in 0...ranked.size
+          if player[i] >= ranked[j]
+            position = positions[j]
+            break
+          end
+        end
+        result << position
+      end
+
+      result
     end
   end
 end
--- climbing_the_leaderboard/spec/climbing_the_leaderboard_spec.rb
+++ climbing_the_leaderboard/spec/climbing_the_leaderboard_spec.rb
@@ -2,16 +2,16 @@
 
 RSpec.describe ClimbingTheLeaderBoard do
   describe '.execute' do
-    context 'when pass scores [100, 90, 90, 80, 75, 60] and players [50, 65, 77, 90, 102]' do
+    context 'when pass ranked [100, 90, 90, 80, 75, 60] and player [50, 65, 77, 90, 102]' do
       subject do
-        ClimbingTheLeaderBoard.execute(scores, players)
+        ClimbingTheLeaderBoard.execute(ranked, player)
       end
 
-      let(:scores) do
+      let(:ranked) do
         [100, 90, 90, 80, 75, 60]
       end
 
-      let(:players) do
+      let(:player) do
         [50, 65, 77, 90, 102]
       end
 
