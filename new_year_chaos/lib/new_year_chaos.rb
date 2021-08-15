063f9fb4adee30eb67a0398102bd61e79303b223
flaviogf <flavio.fernandes6@gmail.com>
2021-08-15 15:15:51 -0300

    new year chaos

--- new_year_chaos/lib/new_year_chaos.rb
+++ new_year_chaos/lib/new_year_chaos.rb
@@ -4,6 +4,20 @@ module NewYearChaos
   module_function
 
   def minimum_bribes(queue)
-    1
+    bribe = 0
+
+    queue.each_with_index do |number, index|
+      expected = index + 1
+
+      next if number <= expected
+
+      current_bribe = number - expected
+
+      return 'Too chaotic' if current_bribe > 2
+
+      bribe += current_bribe
+    end
+
+    bribe
   end
 end
--- new_year_chaos/spec/new_year_chaos_spec.rb
+++ new_year_chaos/spec/new_year_chaos_spec.rb
@@ -13,5 +13,29 @@ RSpec.describe NewYearChaos do
 
       it { is_expected.to eq(1) }
     end
+
+    context 'when pass [4, 1, 2, 3]' do
+      subject do
+        NewYearChaos.minimum_bribes(queue)
+      end
+
+      let(:queue) do
+        [4, 1, 2, 3]
+      end
+
+      it { is_expected.to eq('Too chaotic') }
+    end
+
+    context 'when pass [1, 2, 5, 3, 7, 8, 6, 4]' do
+      subject do
+        NewYearChaos.minimum_bribes(queue)
+      end
+
+      let(:queue) do
+        [1, 2, 5, 3, 7, 8, 6, 4]
+      end
+
+      it { is_expected.to eq(7) }
+    end
   end
 end
