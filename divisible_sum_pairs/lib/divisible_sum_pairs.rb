1b8448126d74f2476f93e9a1bd7425473d41b9e4
flaviogf <flavio.fernandes6@gmail.com>
2021-12-04 09:29:49 -0300

    divisible_sum_pairs

--- divisible_sum_pairs/lib/divisible_sum_pairs.rb
+++ divisible_sum_pairs/lib/divisible_sum_pairs.rb
@@ -4,6 +4,14 @@ module DivisibleSumPairs
   module_function
 
   def execute(n, k, ar)
-    5
+    number_of_pairs = 0
+
+    n.times do |i|
+      n.times do |j|
+        number_of_pairs += 1 if i < j && (ar[i] + ar[j]) % k == 0
+      end
+    end
+
+    number_of_pairs
   end
 end
--- divisible_sum_pairs/spec/divisible_sum_pairs_spec.rb
+++ divisible_sum_pairs/spec/divisible_sum_pairs_spec.rb
@@ -2,13 +2,28 @@
 
 RSpec.describe DivisibleSumPairs do
   describe '.execute' do
-    subject { described_class.execute(n, k, ar) }
+    shared_examples 'expected pairs' do
+      subject { described_class.execute(n, k, ar) }
 
-    let(:n) { 6 }
-    let(:k) { 5 }
-    let(:ar) { [1, 2, 3, 4, 5, 6] }
-    let(:expected_result) { 5 }
+      it { is_expected.to eq (expected_result) }
+    end
 
-    it { is_expected.to eq (expected_result) }
+    context 'when n=6, k=5, ar=[1,2,3,4,5,6]' do
+      let(:n) { 6 }
+      let(:k) { 5 }
+      let(:ar) { [1, 2, 3, 4, 5, 6] }
+      let(:expected_result) { 3 }
+
+      include_examples 'expected pairs'
+    end
+
+    context 'when n=6, k=3, ar=[1,3,2,6,1,2]' do
+      let(:n) { 6 }
+      let(:k) { 3 }
+      let(:ar) { [1, 3, 2, 6, 1, 2] }
+      let(:expected_result) { 5 }
+
+      include_examples 'expected pairs'
+    end
   end
 end
