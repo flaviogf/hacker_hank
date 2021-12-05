4371db803e4a7f3f77645a47a0e4c15ab4b59f2b
flaviogf <flavio.fernandes6@gmail.com>
2021-12-05 08:12:24 -0300

    migratory_birds

--- migratory_birds/lib/migratory_birds.rb
+++ migratory_birds/lib/migratory_birds.rb
@@ -4,6 +4,20 @@ module MigratoryBirds
   module_function
 
   def execute(arr)
-    4
+    most_frequently = arr[0]
+    counter = {}
+
+    arr.each do |bird|
+      counter[bird] ||= 0
+      counter[bird] += 1
+
+      most_frequently = bird if counter[most_frequently] < counter[bird]
+
+      next unless counter[most_frequently] == counter[bird]
+
+      most_frequently = bird if most_frequently > bird
+    end
+
+    most_frequently
   end
 end
--- migratory_birds/spec/migratory_birds_spec.rb
+++ migratory_birds/spec/migratory_birds_spec.rb
@@ -2,12 +2,23 @@
 
 RSpec.describe MigratoryBirds do
   describe '.execute' do
-    context 'when arr=[1, 4, 4, 4, 5, 3]' do
+    shared_examples 'expected result' do
       subject(:result) { described_class.execute(arr) }
+      it { is_expected.to eq(expected_result) }
+    end
+
+    context 'when arr=[1, 4, 4, 4, 5, 3]' do
       let(:arr) { [1, 4, 4, 4, 5, 3] }
       let(:expected_result) { 4 }
 
-      it { is_expected.to eq(expected_result) }
+      include_examples 'expected result'
+    end
+
+    context 'when arr=[1, 2, 3, 4, 5, 4, 3, 2, 1, 3, 4]' do
+      let(:arr) { [1, 2, 3, 4, 5, 4, 3, 2, 1, 3, 4] }
+      let(:expected_result) { 3 }
+
+      include_examples 'expected result'
     end
   end
 end
