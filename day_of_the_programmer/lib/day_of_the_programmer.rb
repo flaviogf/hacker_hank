5f849f5d3d43aa36d8b4e177a175e002f11d9cb1
flaviogf <flavio.fernandes6@gmail.com>
2021-12-08 16:31:21 -0300

    day_of_the_programmer

--- day_of_the_programmer/lib/day_of_the_programmer.rb
+++ day_of_the_programmer/lib/day_of_the_programmer.rb
@@ -1,9 +1,20 @@
 # frozen_string_literal: true
 
 module DayOfTheProgrammer
-  module_function
+  class << self
+    def execute(year)
+      day = number_of_days_in_a_year - sum_of_the_days_of_the_months_until_september
+      "#{day}.09.#{year}"
+    end
 
-  def execute(year)
-    '13.09.2017'
+    private
+
+    def number_of_days_in_a_year
+      256
+    end
+
+    def sum_of_the_days_of_the_months_until_september
+      243
+    end
   end
 end
--- day_of_the_programmer/spec/spec_helper.rb
+++ day_of_the_programmer/spec/spec_helper.rb
@@ -1,5 +1,8 @@
 # frozen_string_literal: true
 
+require 'pry'
+require 'pry-byebug'
+
 require 'day_of_the_programmer'
 
 RSpec.configure do |config|
