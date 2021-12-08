fc6b6738eb433bef5f8e2b5280dab13878f4af0a
flaviogf <flavio.fernandes6@gmail.com>
2021-12-08 16:20:51 -0300

    day_of_the_programmer

--- day_of_the_programmer/spec/day_of_the_programmer_spec.rb
+++ day_of_the_programmer/spec/day_of_the_programmer_spec.rb
@@ -2,12 +2,24 @@
 
 RSpec.describe DayOfTheProgrammer do
   describe '.execute' do
-    context 'when year is 2017' do
+    shared_examples 'expected date' do
       subject(:result) { described_class.execute(year) }
+
+      it { is_expected.to eq(expected_date) }
+    end
+
+    context 'when year is 2017' do
       let(:year) { 2017 }
       let(:expected_date) { '13.09.2017' }
 
-      it { is_expected.to eq(expected_date) }
+      include_examples 'expected date'
+    end
+
+    context 'when year is 2016' do
+      let(:year) { 2016 }
+      let(:expected_date) { '12.09.2016' }
+
+      include_examples 'expected date'
     end
   end
 end
