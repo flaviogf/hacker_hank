0f1ca9f74b61538b623946b689c8a5b1f4ba147d
flaviogf <flavio.fernandes6@gmail.com>
2022-02-12 07:48:56 -0500

    sales by match

--- sales_by_match/go.mod
+++ sales_by_match/go.mod
@@ -1,3 +1,5 @@
 module github.com/flaviogf/dev/hackerrank/sales_by_match
 
 go 1.17
+
+require github.com/gorilla/mux v1.8.0
--- sales_by_match/main.go
+++ sales_by_match/main.go
@@ -1,9 +1,49 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"log"
+	"net"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+
+	"github.com/gorilla/mux"
+)
 
 func main() {
-	fmt.Println("It works")
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	l, err := net.Listen("tcp", ":8080")
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	r := mux.NewRouter()
+
+	s := http.Server{
+		Handler:           r,
+		IdleTimeout:       5 * time.Second,
+		ReadHeaderTimeout: 5 * time.Second,
+	}
+
+	go func() {
+		if err := s.Serve(l); err != nil {
+			log.Println(err)
+		}
+	}()
+
+	signalCh := make(chan os.Signal, 1)
+	signal.Notify(signalCh, syscall.SIGTERM, syscall.SIGINT)
+	<-signalCh
+
+	s.Shutdown(ctx)
+
+	log.Println("Server finished")
 }
 
 func SockMerchant(_ int32, ar []int32) int32 {
