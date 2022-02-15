b20b942ea15ca4ef21f44a791591a8fe68abbdc0
flaviogf <flavio.fernandes6@gmail.com>
2022-02-15 11:55:17 -0500

    drawing book

--- drawing_book/lib/drawing_book.rb
+++ drawing_book/lib/drawing_book.rb
@@ -6,7 +6,15 @@
 module DrawingBook
   class << self
     def page_count(number_of_pages, page_number_to_turn_on)
-      1
+      counter = 0
+
+      (1...number_of_pages).each do |i|
+        break if i == page_number_to_turn_on
+
+        counter += 1
+      end
+
+      counter
     end
   end
 end
--- drawing_book/spec/drawing_book_spec.rb
+++ drawing_book/spec/drawing_book_spec.rb
@@ -3,11 +3,26 @@
 require 'spec_helper'
 
 RSpec.describe DrawingBook do
-  subject { described_class.page_count(number_of_pages, page_number_to_turn_on) }
+  shared_examples 'expected result' do
+    subject { described_class.page_count(number_of_pages, page_number_to_turn_on) }
 
-  let(:number_of_pages) { 6 }
-  let(:page_number_to_turn_on) { 2 }
-  let(:expected_result) { 1 }
 
-  it { is_expected.to eq(expected_result) }
+    it { is_expected.to eq(expected_result) }
+  end
+
+  context 'when number_of_pages=6, page_number_to_turn_on=2' do
+    let(:number_of_pages) { 6 }
+    let(:page_number_to_turn_on) { 2 }
+    let(:expected_result) { 1 }
+
+    include_examples 'expected result'
+  end
+
+  context 'when number_of_pages=5, page_number_to_turn_on=4' do
+    let(:number_of_pages) { 5 }
+    let(:page_number_to_turn_on) { 4 }
+    let(:expected_result) { 0 }
+
+    include_examples 'expected result'
+  end
 end
