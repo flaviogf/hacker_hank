64f955220cee7a8e1708d837d27dbd5371cd5b77
flaviogf <flavio.fernandes6@gmail.com>
2022-02-18 11:18:10 -0500

    drawing book

--- drawing_book/lib/drawing_book.rb
+++ drawing_book/lib/drawing_book.rb
@@ -6,22 +6,37 @@
 module DrawingBook
   class << self
     def page_count(number_of_pages, page_number_to_turn_on)
-      front = {}
+      tracer = OpenTelemetry.tracer_provider.tracer
 
-      (1..number_of_pages).each do |i|
-        front[i] = i / 2
+      tracer.in_span('page_count') do |span|
+        span.add_attributes(
+          'number_of_pages' => number_of_pages.to_s,
+          'page_number_to_turn_on' => page_number_to_turn_on.to_s
+        )
+
+        front = {}
+
+        (1..number_of_pages).each do |i|
+          front[i] = i / 2
+        end
+
+        back = {}
+
+        x = number_of_pages.even? ? 1 : 0
+
+        number_of_pages.downto(1).each do |i|
+          back[i] = x / 2
+          x += 1
+        end
+
+        result = front[page_number_to_turn_on] < back[page_number_to_turn_on] ? front[page_number_to_turn_on] : back[page_number_to_turn_on]
+
+        span.add_attributes(
+          'result' => result.to_s
+        )
+
+        result
       end
-
-      back = {}
-
-      x = number_of_pages.even? ? 1 : 0
-
-      number_of_pages.downto(1).each do |i|
-        back[i] = x / 2
-        x += 1
-      end
-
-      front[page_number_to_turn_on] < back[page_number_to_turn_on] ? front[page_number_to_turn_on] : back[page_number_to_turn_on]
     end
   end
 end
