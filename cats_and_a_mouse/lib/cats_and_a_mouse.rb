d7d16556ddc7b66cd0512d2344642449a3db4934
flaviogf <flavio.fernandes6@gmail.com>
2022-02-27 14:41:46 -0300

    cats and a mouse

---
+++ cats_and_a_mouse/lib/cats_and_a_mouse.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+# This is the CatsAndAMouse main namespace
+module CatsAndAMouse
+  class << self
+    def execute(cat_a, cat_b, mouse)
+      'Cat B'
+    end
+  end
+end
