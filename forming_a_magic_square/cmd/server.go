3230cc62b070932acf9e6093e4ab33d1fe09e43d
flaviogf <flavio.fernandes6@gmail.com>
2022-03-02 11:26:57 -0300

    forming a magic square

--- forming_a_magic_square/cmd/server.go
+++ forming_a_magic_square/cmd/server.go
@@ -1,7 +1,7 @@
 package main
 
-import "fmt"
+import "github.com/flaviogf/hackerrank/forming_a_magic_square/internal/service"
 
 func main() {
-	fmt.Println("It works")
+	service.FormingMagicSquare()
 }
---
+++ forming_a_magic_square/internal/service.go
@@ -0,0 +1,7 @@
+package service
+
+import "fmt"
+
+func FormingMagicSquare() {
+	fmt.Println("OK")
+}
