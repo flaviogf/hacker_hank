f6c83ae367b7cb84130a1222086db79a5baf67e5
flaviogf <flavio.fernandes6@gmail.com>
2022-03-04 12:57:28 -0300

    forming a magic square

--- forming_a_magic_square/internal/services/forming_a_magic_square.go
+++ forming_a_magic_square/internal/services/forming_a_magic_square.go
@@ -1,47 +1,9 @@
 package services
 
-import "math"
-
 const MAGIC_NUMBER = 15
 
 func FormingMagicSquare(s [][]int32) int32 {
 	var totalCost int32
-	usedNumbers := make(map[int32]bool)
-
-	for _, row := range s {
-		for i, value := range row {
-			var rowSum int32
-
-			for _, j := range row {
-				rowSum += j
-			}
-
-			if rowSum == MAGIC_NUMBER {
-				for _, j := range row {
-					usedNumbers[j] = true
-				}
-
-				break
-			}
-
-			incr := MAGIC_NUMBER - rowSum
-			next := value + incr
-
-			if usedNumbers[next] {
-				continue
-			}
-
-			cost := int32(math.Abs(float64(value) - float64(next)))
-			totalCost += cost
-			row[i] = next
-
-			for _, j := range row {
-				usedNumbers[j] = true
-			}
-
-			break
-		}
-	}
 
 	return totalCost
 }
