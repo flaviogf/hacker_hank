866c1162b3dfb3775c6f613f60bd132e10b96520
flaviogf <flavio.fernandes6@gmail.com>
2022-03-04 13:07:17 -0300

    forming a magic square

--- forming_a_magic_square/internal/services/forming_a_magic_square.go
+++ forming_a_magic_square/internal/services/forming_a_magic_square.go
@@ -7,3 +7,13 @@ func FormingMagicSquare(s [][]int32) int32 {
 
 	return cost
 }
+
+func sumRow(s [][]int32, n int32) int32 {
+	var result int32
+
+	for _, value := range s[n] {
+		result += value
+	}
+
+	return result
+}
