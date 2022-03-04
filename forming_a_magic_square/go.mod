a39a749f2aa2e5ffbae47e6f9473e51fd31eecf8
flaviogf <flavio.fernandes6@gmail.com>
2022-03-04 13:18:47 -0300

    forming a magic square

--- forming_a_magic_square/go.mod
+++
@@ -1,3 +0,0 @@
-module github.com/flaviogf/hackerrank/forming_a_magic_square
-
-go 1.17
--- forming_a_magic_square/internal/services/forming_a_magic_square.go
+++
@@ -1,9 +0,0 @@
-package services
-
-const MAGIC = 15
-
-func FormingMagicSquare(s [][]int32) int32 {
-	var cost int32
-
-	return cost
-}
