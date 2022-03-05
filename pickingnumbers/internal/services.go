8d678216e400f3ba9873f787d60d5b3c19330bf8
flaviogf <flavio.fernandes6@gmail.com>
2022-03-05 16:42:28 -0300

    picking numbers

--- pickingnumbers/internal/services.go
+++ pickingnumbers/internal/services.go
@@ -1,5 +1,42 @@
 package services
 
+import (
+	"math"
+	"sort"
+)
+
 func PickingNumbers(a []int32) int32 {
-	return 0
+	sort.Slice(a, func(i, j int) bool { return a[i] < a[j] })
+
+	sets := make([][]int32, len(a))
+
+	for i, n := range a {
+		for _, v := range a {
+			diff := int32(math.Abs(float64(n) - float64(v)))
+
+			if diff > 1 {
+				continue
+			}
+
+			if len(sets[i]) != 0 {
+				diff := int32(math.Abs(float64(sets[i][0]) - float64(v)))
+
+				if diff > 1 {
+					continue
+				}
+			}
+
+			sets[i] = append(sets[i], v)
+		}
+	}
+
+	var max int32
+
+	for _, s := range sets {
+		if int32(len(s)) > max {
+			max = int32(len(s))
+		}
+	}
+
+	return max
 }
