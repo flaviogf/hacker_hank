ddfd4f6cc9c2451ec994420271af4c9e85b3ba33
flaviogf <flavio.fernandes6@gmail.com>
2022-03-05 15:06:50 -0300

    picking numbers

---
+++ pickingnumbers/internal/services_test.go
@@ -0,0 +1,21 @@
+package services
+
+import "testing"
+
+func TestPickingNumbers(t *testing.T) {
+	testTable := []struct {
+		Input          []int32
+		ExpectedResult int32
+	}{
+		{Input: []int32{4, 6, 5, 3, 3, 1}, ExpectedResult: 3},
+		{Input: []int32{1, 2, 2, 3, 1, 2}, ExpectedResult: 5},
+	}
+
+	for i, tt := range testTable {
+		result := PickingNumbers(tt.Input)
+
+		if tt.ExpectedResult != result {
+			t.Errorf("#%d want: %d, got: %d\n", i, tt.ExpectedResult, result)
+		}
+	}
+}
