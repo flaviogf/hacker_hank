a3e16af6447d3f63e7bf2a0fe07f710bc5638816
flaviogf <flavio.fernandes6@gmail.com>
2022-03-11 13:30:34 -0300

    hurdlerace

--- hurdlerace/internal/hurdlerace_test.go
+++ hurdlerace/internal/hurdlerace_test.go
@@ -9,13 +9,14 @@ func TestHurdlaRace(t *testing.T) {
 		ExpectedResult int32
 	}{
 		{K: 4, Height: []int32{1, 6, 3, 5, 2}, ExpectedResult: 2},
+		{K: 7, Height: []int32{2, 5, 4, 5, 2}, ExpectedResult: 0},
 	}
 
 	for i, tt := range tableTests {
 		result := HurdleRace(tt.K, tt.Height)
 
 		if tt.ExpectedResult != result {
-			t.Errorf("#%d got: %d want: %d", i, tt.ExpectedResult, result)
+			t.Errorf("#%d want: %d got: %d", i, tt.ExpectedResult, result)
 		}
 	}
 }
