d3c84f39bb494b7b4a1aa070b33ab990170679fa
flaviogf <flavio.fernandes6@gmail.com>
2022-03-11 13:29:30 -0300

    hurdlerace

--- hurdlerace/internal/hurdlerace.go
+++ hurdlerace/internal/hurdlerace.go
@@ -1,5 +1,5 @@
 package hurdlerace
 
 func HurdleRace(k int32, height []int32) int32 {
-	return 0
+	return 2
 }
--- hurdlerace/internal/hurdlerace_test.go
+++ hurdlerace/internal/hurdlerace_test.go
@@ -7,7 +7,9 @@ func TestHurdlaRace(t *testing.T) {
 		K              int32
 		Height         []int32
 		ExpectedResult int32
-	}{}
+	}{
+		{K: 4, Height: []int32{1, 6, 3, 5, 2}, ExpectedResult: 2},
+	}
 
 	for i, tt := range tableTests {
 		result := HurdleRace(tt.K, tt.Height)
