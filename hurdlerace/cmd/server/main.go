e5b9cdc4c5375d02291bb3f63539cedaf05b6e2b
flaviogf <flavio.fernandes6@gmail.com>
2022-03-12 14:37:08 -0300

    hurdlerace

--- hurdlerace/cmd/server/main.go
+++ hurdlerace/cmd/server/main.go
@@ -1,7 +1,45 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"log"
+	"net"
+	"net/http"
+	"os"
+	"time"
+
+	"github.com/gorilla/mux"
+)
 
 func main() {
-	fmt.Println("It works")
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	l, err := net.Listen("tcp", "0.0.0.0:8080")
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	r := mux.NewRouter()
+
+	s := http.Server{
+		Handler:           r,
+		IdleTimeout:       5 * time.Second,
+		ReadHeaderTimeout: 5 * time.Second,
+	}
+
+	doneCh := make(chan os.Signal, 1)
+
+	go func() {
+		if err := s.Serve(l); err != nil {
+			log.Println(err)
+		}
+	}()
+
+	log.Println("Server listening on port 8080")
+
+	<-doneCh
+
+	s.Shutdown(ctx)
 }
--- hurdlerace/go.mod
+++ hurdlerace/go.mod
@@ -1,3 +1,5 @@
 module github.com/flaviogf/hackerrank/hurdlerace
 
 go 1.17
+
+require github.com/gorilla/mux v1.8.0
