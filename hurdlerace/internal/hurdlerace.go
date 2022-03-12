b8ad94d8b9ce91d4d5881d7c4d4cd109b7971754
flaviogf <flavio.fernandes6@gmail.com>
2022-03-12 14:31:04 -0300

    hurdlerace

--- hurdlerace/internal/hurdlerace.go
+++ hurdlerace/internal/hurdlerace.go
@@ -1,5 +1,16 @@
 package hurdlerace
 
+import "sort"
+
 func HurdleRace(k int32, height []int32) int32 {
-	return 2
+	sort.Slice(height, func(i, j int) bool { return height[i] < height[j] })
+
+	max := height[len(height)-1]
+	diff := int32(max - k)
+
+	if diff > 0 {
+		return diff
+	}
+
+	return 0
 }
